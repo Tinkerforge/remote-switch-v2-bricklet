5df8efa2a73fa80a064cf347e05dc75b1860393f
Olaf Lüke <olaf@tinkerforge.com>
2017-09-18 17:40:16 +0200

    Remove debug

--- software/src/main.c
+++ software/src/main.c
@@ -26,15 +26,11 @@
 
 #include "bricklib2/bootloader/bootloader.h"
 #include "bricklib2/hal/system_timer/system_timer.h"
-#include "bricklib2/logging/logging.h"
 #include "communication.h"
 #include "rfm69.h"
 
 
 int main(void) {
-	logging_init();
-	logd("Start Remote Switch V2 Bricklet\n\r");
-
 	communication_init();
 	rfm69_init();
 
