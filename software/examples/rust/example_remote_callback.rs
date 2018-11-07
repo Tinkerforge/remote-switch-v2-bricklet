7dde1b0e2cfb461a4b1de3a2e18a5fd81cd510bd
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:26:34 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_remote_callback.rs
+++ software/examples/rust/example_remote_callback.rs
@@ -1,30 +1,31 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, remote_switch_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, remote_switch_v2_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Remote Switch Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Remote Switch Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let remote_switch_v2_bricklet = RemoteSwitchV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let rs = RemoteSwitchV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Configure to receive from remote type A with minimum repeats set to 1 and enable callback
-    remote_switch_v2_bricklet.set_remote_configuration(REMOTE_SWITCH_V2_BRICKLET_REMOTE_TYPE_A, 1, true);
+    rs.set_remote_configuration(REMOTE_SWITCH_V2_BRICKLET_REMOTE_TYPE_A, 1, true);
 
-    //Create listener for remote status a events.
-    let remote_status_a_listener = remote_switch_v2_bricklet.get_remote_status_a_receiver();
-    // Spawn thread to handle received events. This thread ends when the remote_switch_v2_bricklet
+    // Create receiver for remote status a events.
+    let remote_status_a_receiver = rs.get_remote_status_a_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `rs` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in remote_status_a_listener {
-            println!("House Code: {}", event.house_code);
-            println!("Receiver Code: {}", event.receiver_code);
-            println!("Switch To: {}", event.switch_to);
-            println!("Repeats: {}", event.repeats);
+        for remote_status_a in remote_status_a_receiver {
+            println!("House Code: {}", remote_status_a.house_code);
+            println!("Receiver Code: {}", remote_status_a.receiver_code);
+            println!("Switch To: {}", remote_status_a.switch_to);
+            println!("Repeats: {}", remote_status_a.repeats);
             println!();
         }
     });
