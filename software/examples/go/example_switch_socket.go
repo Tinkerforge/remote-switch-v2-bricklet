2d4a6ed3d681542a0f2fbe5c43c4c6f7b97ccb78
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:00:02 +0100

    go: Change import path.

--- software/examples/go/example_switch_socket.go
+++ software/examples/go/example_switch_socket.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/remote_switch_v2_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/remote_switch_v2_bricklet"
 )
 
 const ADDR string = "localhost:4223"
