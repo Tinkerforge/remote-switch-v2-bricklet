8f72af9bd27e10a8cdd15728650848682de65644
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:58:39 +0100

    rust: Reformat examples.

--- software/examples/rust/example_remote_callback.rs
+++ software/examples/rust/example_remote_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  remote_switch_v2_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, remote_switch_v2_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,7 +10,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rs = RemoteSwitchV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-<<<<<<< Updated upstream
                                           // Don't use device before ipcon is connected.
 
     // Configure to receive from remote type A with minimum repeats set to 1 and enable callback
@@ -39,34 +35,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             println!();
         }
     });
-=======
-    // Don't use device before ipcon is connected.
-
-		// Configure to receive from remote type A with minimum repeats set to 1 and enable callback
-		rs.set_remote_configuration(REMOTE_SWITCH_V2_BRICKLET_REMOTE_TYPE_A, 1, true);
-
-     let remote_status_a_receiver = rs.get_remote_status_a_callback_receiver();
-
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `rs` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for remote_status_a in remote_status_a_receiver {           
-                		println!("House Code: {}", remote_status_a.house_code);
-		println!("Receiver Code: {}", remote_status_a.receiver_code);
-
-		if remote_status_a.switch_to == REMOTE_SWITCH_V2_BRICKLET_SWITCH_TO_OFF { 
-			println!("Switch To: Off");
-		}
-		else if remote_status_a.switch_to == REMOTE_SWITCH_V2_BRICKLET_SWITCH_TO_ON { 
-			println!("Switch To: On");
-		}
-
-		println!("Repeats: {}", remote_status_a.repeats);
-		println!();
-            }
-        });
->>>>>>> Stashed changes
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_switch_socket.rs
+++ software/examples/rust/example_switch_socket.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  remote_switch_v2_bricklet::*};
-
+use tinkerforge::{ip_connection::IpConnection, remote_switch_v2_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,14 +11,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rs = RemoteSwitchV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Switch on a type A socket with house code 17 and receiver code 1.
-		// House code 17 is 10001 in binary (least-significant bit first)
-		// and means that the DIP switches 1 and 5 are on and 2-4 are off.
-		// Receiver code 1 is 10000 in binary (least-significant bit first)
-		// and means that the DIP switch A is on and B-E are off.
-		rs.switch_socket_a(17, 1, REMOTE_SWITCH_V2_BRICKLET_SWITCH_TO_ON);
+    // Switch on a type A socket with house code 17 and receiver code 1.
+    // House code 17 is 10001 in binary (least-significant bit first)
+    // and means that the DIP switches 1 and 5 are on and 2-4 are off.
+    // Receiver code 1 is 10000 in binary (least-significant bit first)
+    // and means that the DIP switch A is on and B-E are off.
+    rs.switch_socket_a(17, 1, REMOTE_SWITCH_V2_BRICKLET_SWITCH_TO_ON);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
