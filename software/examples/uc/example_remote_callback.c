36d7a177bf87db3291a1536513d0300d2c32c620
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:38:07 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_remote_callback.c
+++ software/examples/uc/example_remote_callback.c
@@ -1,17 +1,13 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_remote_switch_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_remote_switch_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Remote Switch Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 // Callback function for remote status a callback
 static void remote_status_a_handler(TF_RemoteSwitchV2 *device, uint8_t house_code,
@@ -36,7 +32,7 @@ static TF_RemoteSwitchV2 rs;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_remote_switch_v2_create(&rs, UID, hal), "create device object");
+	check(tf_remote_switch_v2_create(&rs, NULL, hal), "create device object");
 
 	// Configure to receive from remote type A with minimum repeats set to 1 and enable callback
 	check(tf_remote_switch_v2_set_remote_configuration(&rs,
